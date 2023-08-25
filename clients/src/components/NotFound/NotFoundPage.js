dffcebf3dbfccf7664359a6019451e7ede656786
kamran arshad <kamranarshad.spyko@gmail.com>
2023-08-26 00:45:28 +0500

    updates!!! from Abdul Hadi

--- clients/src/components/NotFound/NotFoundPage.js
+++ clients/src/components/NotFound/NotFoundPage.js
@@ -8,7 +8,7 @@ function NotFoundPage() {
             <h1 className="text-6xl font-bold text-red-500">404</h1>
             <p className="text-xl text-gray-600 mt-4">Page Not Found</p>
             <p className="text-gray-500 mt-2">Oops! The page you are looking for does not exist.</p>
-            <Link to="/mainDashboard" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold mt-4 px-6 py-2 rounded-lg transition-colors duration-300">Go Back Home</Link>
+            <Link to="/mainDashboard" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold mt-4 px-6 py-2 rounded-lg transition-colors duration-300">Go Back Dashboard</Link>
         </div>
     </div>
   )
