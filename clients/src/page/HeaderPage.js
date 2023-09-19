96b415299c038fd8d1b2debf8484f834c137fdb0
Abdul Hadi <abdul.806@yahoo.com>
2023-09-20 01:26:51 +0500

    updates!!!

--- clients/src/page/HeaderPage.js
+++ clients/src/page/HeaderPage.js
@@ -8,9 +8,11 @@ import {
   LogoutAdminHandler,
   UserToken,
 } from "../redux/utils/adminAuth";
-
+import alertify from "alertifyjs";
+import "alertifyjs/build/css/alertify.css";
 import Header from "../components/Header";
 import { io } from "socket.io-client";
+
 function HeaderPage({ role }) {
   const testToken = UserToken();
   const { data: adminProfile, refetch: getAdminProfile } = useGetAdminProfileQuery(testToken);
@@ -22,6 +24,8 @@ function HeaderPage({ role }) {
   
   useEffect(() => { 
     socket.on(`new_Task_Update_to_${userProfile && userProfile.user._id.toUpperCase()}`, (data) => {
+      alertify.set("notifier", "position", "top-right");
+          alertify.success(data.message);
       getAllUserTasks();
     });
     return () => {
